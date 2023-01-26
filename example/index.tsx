c7c9ac2569a6a6cbcaa72a41213c329cc9d5755e
CIAO <you@example.com>
2023-01-25 19:30:05 -0500

    listo

--- example/index.tsx
+++ example/index.tsx
@@ -6,27 +6,6 @@ import { ProductCard } from '../src/components/productCard/index';
 const App = () => {
   return (
     <div>
-      <ProductCard 
-        initialValues={{
-          count:3,
-          maxCount:10
-        }}
-      >
-        {
-          ({reset, increaseBy, count}) => (
-            <>
-              <ProductCard.Img/>
-              <ProductCard.Title title='hola mundo' />
-              <ProductCard.Buttons />
-
-              <button onClick={reset}>rese</button>
-              <button onClick={() => increaseBy(-2)}>-2</button>
-              <button  onClick={() => increaseBy(+2)}>+2</button>
-              <span>{count}</span>
-            </>
-          )
-        }
-      </ProductCard>
     </div>
   );
 };
